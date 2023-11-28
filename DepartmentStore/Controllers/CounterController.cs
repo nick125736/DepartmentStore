be84f7d15403ec4b17d9372918793ef2090b81a2
110b26379 <110b26379@mailst.cjcu.edu.tw>
2023-11-28 13:51:19 +0800

    push

--- DepartmentStore/Controllers/CounterController.cs
+++ DepartmentStore/Controllers/CounterController.cs
@@ -6,41 +6,6 @@ namespace DepartmentStore.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CrossController : ControllerBase
-    {
-        // 記錄和追蹤應用程式的運行時行為及各種訊息，如警告、錯誤等
-        private readonly ILogger<CrossController> _logger;
-        // 宣告此控制器所要依賴的介面（Interface），而不是其實作
-        private readonly ICross _cross;
-        public CrossController(ILogger<CrossController> logger, ICross cross)
-        {
-            // 注入 Logger 服務
-            _logger = logger;
-            // 注入 Cross 服務
-            _cross = cross;
-        }
-        [HttpGet("MemberForCounter/{cid}")]
-        public async Task<IActionResult> GetCalendarsByMemberId(Guid cid)
-        {
-            try
-            {
-                // 取得指定 id 的會員資料
-                var counter = await _cross.GetMemberByCounterId(cid);
-                return Ok(new
-                {
-                    Success = true,
-                    Message = "輸出counter的id,name且對應member",
-                    counter
-                });
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-        }
-    }
-    [Route("api/[controller]")]
-    [ApiController]
     public class CounterController: ControllerBase
     {
         private readonly ILogger<CounterController> _logger;
